ac8d6775ce4aaca75280ece2755845c29089a789
GYALLERHORN <kjins1998@gmail.com>
2023-08-10 17:00:01 +0900

    스타트씬에서 쓸 브금 변수, 에셋 추가

--- 4_Team_Card_Game/Assets/Scripts/audioManager.cs
+++ 4_Team_Card_Game/Assets/Scripts/audioManager.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class audioManager : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip bgm;
+    public AudioClip startSceneSource;
+    public AudioClip mainSceneBgm;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip = bgm;
+        audioSource.clip = mainSceneBgm;
         audioSource.Play();
     }
 
